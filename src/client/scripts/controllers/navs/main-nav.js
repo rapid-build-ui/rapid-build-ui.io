ff7f9a143aa909cb364f30c3fa4ede480b5bb664
Judd Younce <jyounce@yahoo.com>
2017-11-18 23:07:46 -0600

    docs(nav): order alphabetically

--- src/client/scripts/controllers/navs/main-nav.js
+++ src/client/scripts/controllers/navs/main-nav.js
@@ -3,11 +3,10 @@ angular.module('showcase').controller('mainNavController', ['$scope', '$location
 		/* Public
 		 *********/
 		$scope.links  = [
-			{ caption: 'home',    url: '/' },
-			{ caption: 'rb-icon', url: '/components/rb-icon' },
-			{ caption: 'rb-nav',  url: '/components/rb-nav' },
-			{ caption: 'rb-button',  url: '/components/rb-button' }
-
+			{ caption: 'home',      url: '/' },
+			{ caption: 'rb-button', url: '/components/rb-button' },
+			{ caption: 'rb-icon',   url: '/components/rb-icon' },
+			{ caption: 'rb-nav',    url: '/components/rb-nav' }
 		]
 
 		/* Private
