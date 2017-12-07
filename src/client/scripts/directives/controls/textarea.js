8c649775055ef507ca31359dc947888c9d0d645c
Judd Younce <jyounce@yahoo.com>
2017-12-06 22:25:50 -0600

    feat(rba-textarea): add it

---
+++ src/client/scripts/directives/controls/textarea.js
@@ -0,0 +1,31 @@
+angular.module('rapid-build').directive('rbaTextarea', [() => {
+	/* COMPILE
+	 **********/
+	var Compile = function(tElement, tAttrs, transclude) {
+		if (typeof tAttrs.caption === 'undefined')
+			tElement[0].querySelector('.caption').remove();
+
+		return Link;
+	}
+
+	/* LINK
+	 *******/
+	var Link = (scope, iElement, iAttrs) => {
+		scope.name = scope.name || iAttrs.model;
+	}
+
+	/* API
+	 ******/
+	return {
+		compile: Compile,
+		restrict: 'E',
+		templateUrl: '/views/directives/controls/textarea.html',
+		scope: {
+			model:    '=',
+			name:     '@',
+			caption:  '@',
+			inline:   '@',
+			vertical: '@'
+		}
+	};
+}]);
