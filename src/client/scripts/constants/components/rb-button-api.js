eb8b7353b4571b4a6bf3a1d73e52bd916b5c34cc
ayounce <crisan_alina2003@yahoo.com>
2018-03-08 00:10:26 -0600

    feat(rb-button): add incomplete api table

---
+++ src/client/scripts/constants/components/rb-button-api.js
@@ -0,0 +1,38 @@
+angular.module('rapid-build').constant('RB_BUTTON_API', [
+	{
+		name: "type",
+		description: `Comming soon...`
+	},
+	{
+		name: "kind",
+		description: `Comming soon...`
+	},
+	{
+		name: "size",
+		description: `Comming soon...`
+	},
+	{
+		name: "disabled",
+		description: `Comming soon...`
+	},
+	{
+		name: "content",
+		description: `Comming soon...`
+	},
+	{
+		name: "icon",
+		description: `Comming soon...`
+	},
+	{
+		name: "icon-source",
+		description: `Comming soon...`
+	},
+	{
+		name: "icon-position",
+		description: `Coming soon...`
+	},
+	{
+		name: "icon-size",
+		description: `Coming soon...`
+	}
+]);
