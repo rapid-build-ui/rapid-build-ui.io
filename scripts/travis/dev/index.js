37b5888f5829710e470d5b8b333da82f8277e38e
Judd Younce <jyounce@yahoo.com>
2018-05-13 15:02:00 -0500

    chore(travis): test clone component repos via node

--- scripts/travis/dev/index.js
+++ scripts/travis/dev/index.js
@@ -1,7 +1,7 @@
 /*****************************
  * TRAVIS STEPS FOR DEV BUILD
  *****************************/
-require('../../bootstrap/colors');
+// require('../../bootstrap/colors');
 const args  = process.argv.slice(2);
 const paths = {
 	showcase:   args[0],
@@ -9,6 +9,7 @@ const paths = {
 };
 const components   = require('./get-components')(paths);
 const { execSync } = require('child_process');
+console.log(components);
 
 /* CLONE COMPONENT REPOS
  ************************/
@@ -17,12 +18,11 @@ const cloneOpts = { cwd: paths.components };
 for (const repoName of components.repoNames) {
 	let cmd    = `${CLONE_CMD} ${repoName}`;
 	let result = execSync(cmd, cloneOpts).toString();
-	console.info(result.minor);
+	console.info(result);
 }
 
 const resultX = execSync('ls -a1', cloneOpts).toString();
-console.info(resultX.minor);
-console.log(components);
+console.info(resultX);
 console.log(process.cwd());
 
 /* EXIT FOR TESTING
