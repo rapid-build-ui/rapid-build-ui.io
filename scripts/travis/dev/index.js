d18576a5d85e99b8cc4f073e830af242d7817684
Judd Younce <jyounce@yahoo.com>
2018-05-13 17:09:14 -0500

    chore(travis): complete dev node script

--- scripts/travis/dev/index.js
+++ scripts/travis/dev/index.js
@@ -5,8 +5,7 @@ require('../../bootstrap/colors');
 const args  = process.argv.slice(2);
 const paths = {
 	showcase:   args[0],
-	components: args[1],
-	yarnLink:   args[2]
+	components: args[1]
 };
 const components   = require('./get-components')(paths);
 const { execSync } = require('child_process');
@@ -16,7 +15,7 @@ const { execSync } = require('child_process');
 const cloneCmd  = 'git clone --depth 1'
 const cloneOpts = { cwd: paths.components, stdio: [0,1,2] };
 for (const [i, repoName] of components.repoNames.entries()) {
-	console.info(`cloning ${components.names[i]}`.toUpperCase().attn);
+	console.info(`cloning ${components.names[i]}`.toUpperCase().alert);
 	let cmd = `${cloneCmd} ${repoName}`;
 	execSync(cmd, cloneOpts); console.log();
 }
@@ -26,29 +25,21 @@ for (const [i, repoName] of components.repoNames.entries()) {
 const setupCmd  = 'rapid-build prod publish && npm run link'
 const setupOpts = { stdio: [0,1,2] };
 for (const name of components.names) {
-	console.info(`setup component ${name}`.toUpperCase().attn);
+	console.info(`setup component ${name}`.toUpperCase().alert);
 	setupOpts.cwd = `${paths.components}/${name}`
 	execSync(setupCmd, setupOpts); console.log();
 }
 
-/* Setup Showcase and Build
- ***************************/
-const showcaseOpts = { cwd: paths.showcase, stdio: [0,1,2] };
+/* Setup Showcase
+ *****************/
 const showcaseSetupCmd = 'npm run setup'
-const showcaseBuildCmd = 'rapid-build prod publish'
+const showcaseOpts     = { cwd: paths.showcase, stdio: [0,1,2] };
+console.info(`setup showcase`.toUpperCase().alert);
 execSync(showcaseSetupCmd, showcaseOpts); console.log();
-execSync(showcaseBuildCmd, showcaseOpts); console.log();
 
-/* Extra Logging
- ****************/
-// execSync('ls -a1', { cwd: paths.components, stdio: [0,1,2] });
-// console.log();
-//
-// execSync('ls -a1', { cwd: `${paths.yarnLink}/@rapid-build-ui`, stdio: [0,1,2] });
-// console.log();
-//
-// console.log(process.cwd());
-
-/* EXIT FOR TESTING
- *******************/
-// process.exit(1);
+/* Build Showcase
+ *****************/
+const buildCmd  = 'rapid-build prod publish'
+const buildOpts = { cwd: paths.showcase, stdio: [0,1,2] };
+console.info(`build showcase`.toUpperCase().alert);
+execSync(buildCmd, buildOpts); console.log();
