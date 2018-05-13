5bcc658245681e5f0c3fabed6d4dcecd7e5f3719
Judd Younce <jyounce@yahoo.com>
2018-05-13 14:53:00 -0500

    chore(travis): clone component repos via node

--- scripts/travis/dev/index.js
+++ scripts/travis/dev/index.js
@@ -7,9 +7,22 @@ const paths = {
 	showcase:   args[0],
 	components: args[1]
 };
-const components = require('./get-components')(paths);
+const components   = require('./get-components')(paths);
+const { execSync } = require('child_process');
+
+/* CLONE COMPONENT REPOS
+ ************************/
+// process.chdir(paths.components);
+const CLONE_CMD = 'git clone --depth 1'
+const cloneOpts = { cwd: paths.components };
+for (const repoName of components.repoNames) {
+	let cmd    = `${CLONE_CMD} ${repoName}`;
+	let result = execSync(cmd, cloneOpts).toString();
+	console.info(result.minor);
+}
 
 console.log(components);
+console.log(process.cwd());
 
 /* EXIT FOR TESTING
  *******************/
