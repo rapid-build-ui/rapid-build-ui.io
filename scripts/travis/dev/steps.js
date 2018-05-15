c80126265e8bfcbcf9eeffd6717acb4b77dfdb0d
Judd Younce <jyounce@yahoo.com>
2018-05-14 23:55:59 -0500

    chore(travis): try to clone components asynchronously

--- scripts/travis/dev/steps.js
+++ scripts/travis/dev/steps.js
@@ -48,7 +48,7 @@ const Steps = (paths, components) => { // :{}
 					console.log('RESULT:', result);
 					return result;
 				}).catch(error => {
-					console.error('ERROR:', error);
+					console.error('ERROR:\n'.error, error);
 					process.exit(1);
 					return error;
 				});
