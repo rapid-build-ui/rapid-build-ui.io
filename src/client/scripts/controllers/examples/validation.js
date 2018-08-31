4d0a4c82a0688048f2af74e5b426339deaf2de86
yokun <yevgeniyokun@gmail.com>
2018-08-31 01:50:22 -0500

    feat(form validation): add html for the example form

--- src/client/scripts/controllers/examples/validation.js
+++ src/client/scripts/controllers/examples/validation.js
@@ -1,4 +1,21 @@
 angular.module('rapid-build').controller('validationController', ['$scope', '$element', 'rbEventService',
 	($scope, $element, rbEvents) => {
 
+		// Builder
+		// =======
+
+		$scope.markup =  `
+<form name="demoForm" action="/examples/validation">
+	<rb-input
+		label="First Name"
+		validation='[
+			"required"
+		]'>
+	</rb-input>
+
+	<button type="submit">Submit</button>
+</form>
+		`.trim();
+
+
 }]);
