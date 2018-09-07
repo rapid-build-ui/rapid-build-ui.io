43005804fabc3f6b06ee8428ea83a69a5eae24da
Rapid Build UI <rapidbuildui@yahoo.com>
2018-09-07 02:47:15 -0500

    chore(rb components): load them via rapid.js instead of in spa.html

---
+++ src/client/scripts/rapid.js
@@ -0,0 +1,11 @@
+/***********************
+ * IMPORT RB COMPONENTS
+ ***********************/
+import '/node_modules/@rapid-build-ui/rb-alert/scripts/rb-alert.js';
+import '/node_modules/@rapid-build-ui/rb-button/scripts/rb-button.js';
+import '/node_modules/@rapid-build-ui/rb-icon/scripts/rb-icon.js';
+import '/node_modules/@rapid-build-ui/rb-input/scripts/rb-input.js';
+import '/node_modules/@rapid-build-ui/rb-modal/scripts/rb-modal.js';
+import '/node_modules/@rapid-build-ui/rb-nav/scripts/rb-nav.js';
+import '/node_modules/@rapid-build-ui/rb-popover/scripts/rb-popover.js';
+import '/node_modules/@rapid-build-ui/rb-radios/scripts/rb-radios.js';
