d8b5a25126973fe4f2bb63fc55e5ad68e1fded52
Judd Younce <jyounce@yahoo.com>
2018-10-27 05:38:40 -0500

    perf(builder): add a debounce for better performance

--- src/client/scripts/directives/builder.js
+++ src/client/scripts/directives/builder.js
@@ -10,23 +10,27 @@ angular.module('rapid-build').directive('rbaBuilder', ['$compile', 'idService', 
 			let timer      = null;
 			let oldScope   = null;
 			let canDestroy = false;
+			let debounce   = null;
 
-			const builderWatch = scope.$watch(buildStr, (newVal, oldVal) => {
-				if (newVal === void 0) return;
+			const clearDebounce = () => {
+				if (!debounce) return;
+				$timeout.cancel(debounce);
+				debounce = null;
+			}
 
+			const builder = (newVal, oldVal) => {
+				clearDebounce();
 				let elmBuilt = document.getElementById(id);
 				if (elmBuilt) angular.element(elmBuilt).replaceWith(iElement);
-
+				
 				let template = newVal.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
 					template = `<div id="${id}">${template}</div>`;
-
 				let elmTemplate = angular.element(template);
-				let elmChild    = elmTemplate.children().eq(0);
 				let elmChildren = elmTemplate[0].childNodes;
-				if (elmChildren.length === 1 && elmChildren[0].nodeType === 1) {
-					elmChild.attr('id', id);
-					template = elmChild[0].outerHTML;
-				}
+				if (elmChildren.length === 1 && elmChildren[0].nodeType === 1)
+					template = elmTemplate.children().eq(0).attr('id', id)[0].outerHTML;
+
+				elmBuilt = elmTemplate = elmChildren = null; // cleanup
 
 				const quotationMarks = template.match(/"/g);
 				const isValid        = !!quotationMarks && quotationMarks.length % 2 === 0;
@@ -45,12 +49,18 @@ angular.module('rapid-build').directive('rbaBuilder', ['$compile', 'idService', 
 						canDestroy = oldScope && scope.$id !== oldScope.$id;
 					});
 				});
+			}
 
-				elmBuilt = elmTemplate = elmChild = elmChildren = null;
+			const builderWatch = scope.$watch(buildStr, (newVal, oldVal) => {
+				clearDebounce();
+				if (newVal === void 0) return;
+				if (newVal === oldVal) return builder(newVal, oldVal);
+				debounce = $timeout(builder, 50, false, newVal, oldVal);
 			});
 
 			const destroy = scope.$on('$destroy', () => {
 				if (timer) $timeout.cancel(timer);
+				clearDebounce();
 				builderWatch();
 				destroy();
 			});
