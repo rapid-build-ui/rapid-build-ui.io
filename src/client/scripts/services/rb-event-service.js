6846d9ae40cca50e584719c58799f7fc935fe267
Judd Younce <jyounce@yahoo.com>
2018-11-03 17:33:04 -0500

    fix(resetting checkbox): when clicking the reset button on the demo forms

--- src/client/scripts/services/rb-event-service.js
+++ src/client/scripts/services/rb-event-service.js
@@ -1,63 +1,66 @@
 /*******************
  * RB EVENT SERVICE
  *******************/
-angular.module('rapid-build').service('rbEventService', ['$filter', '$parse', function($filter, $parse) {
-	/* Private Props
-	 ****************/
-	const SCOPE_PROP = 'rbNgScopeProp'; // utilizes element dataset
+angular.module('rapid-build').service('rbEventService', ['$filter', '$parse', 'typeService',
+	function($filter, $parse, type) {
+		/* Private Props
+		 ****************/
+		const SCOPE_PROP = 'rbNgScopeProp'; // utilizes element dataset
 
-	/* Private Methods
-	 ******************/
-	const setElmAttr = function(attr, val) { // :void (rb element is this)
-		if (typeof val !== 'string')
-			return this.removeAttribute(attr);
-		this.setAttribute(attr, val);
-	}
+		/* Private Methods
+		 ******************/
+		const setElmAttr = function(attr, val) { // :void (rb element is this)
+			if (val === void 0)
+				return this.removeAttribute(attr);
+			if (type.is.object(val) || type.is.array(val))
+				val = JSON.stringify(val);
+			this.setAttribute(attr, val);
+		}
 
-	const watchElmScopeProp = function(scope, evt) { // :void (rb element is this)
-		const attr = evt.type.split('-').slice(0,-1).join('-'); // ex: value-changed -> value
-		let prop = this.getAttribute(attr);   // ex: a.kind
-			prop = $filter('camelize')(prop); // ex: a.icon-size -> a.iconSize
-		if (!prop) return;
+		const watchElmScopeProp = function(scope, evt) { // :void (rb element is this)
+			const attr = evt.type.split('-').slice(0,-1).join('-'); // ex: value-changed -> value
+			let prop = this.getAttribute(attr);   // ex: a.kind
+				prop = $filter('camelize')(prop); // ex: a.icon-size -> a.iconSize
+			if (!prop) return;
 
-		this.dataset[SCOPE_PROP] = prop;        // ex: a.kind
-		const val = $parse(prop)(scope);      // ex: a.kind -> heart
-		setElmAttr.bind(this, attr, val)();
+			this.dataset[SCOPE_PROP] = prop; // ex: a.kind
+			const val = $parse(prop)(scope); // ex: a.kind -> heart
+			setElmAttr.bind(this, attr, val)();
 
-		const scopePropWatch = scope.$watch(this.dataset[SCOPE_PROP], (newVal, oldVal) => {
-			if (newVal === oldVal) return;
-			setElmAttr.bind(this, attr, newVal)();
-		});
-		scope.$on('$destroy', () => {
-			scopePropWatch();
-		});
-	}
-
-	const eventHandler = function(evt) { // :void (rb element is this)
-		const scope = angular.element(this).scope();
-		if (!this.dataset[SCOPE_PROP])
-			return watchElmScopeProp.bind(this, scope, evt)();
-		$parse(this.dataset[SCOPE_PROP]).assign(scope, evt.detail.value);
-		scope.$apply();
-	}
-
-	const manageEvents = (rbElms, rbEvtName) => { // :void
-		for (const rbElm of rbElms) {
-			rbElm.addEventListener(rbEvtName, eventHandler);
-			angular.element(rbElm).scope().$on('$destroy', () => {
-				rbElm.removeEventListener(rbEvtName, eventHandler);
+			const scopePropWatch = scope.$watch(this.dataset[SCOPE_PROP], (newVal, oldVal) => {
+				if (newVal === oldVal) return;
+				setElmAttr.bind(this, attr, newVal)();
+			});
+			scope.$on('$destroy', () => {
+				scopePropWatch();
 			});
 		}
-	}
 
-	/* Public
-	 *********/
-	this.addListeners = (rbElms, rbEvtName) => { // :void
-		manageEvents(rbElms, rbEvtName);
-	}
+		const eventHandler = function(evt) { // :void (rb element is this)
+			const scope = angular.element(this).scope();
+			if (!this.dataset[SCOPE_PROP])
+				return watchElmScopeProp.bind(this, scope, evt)();
+			$parse(this.dataset[SCOPE_PROP]).assign(scope, evt.detail.value);
+			scope.$apply();
+		}
 
-	this.addListenersFind = ($elmContainer, rbElmName, rbEvtName) => { // :void
-		const rbElms = $elmContainer.querySelectorAll(rbElmName);
-		manageEvents(rbElms, rbEvtName);
-	}
+		const manageEvents = (rbElms, rbEvtName) => { // :void
+			for (const rbElm of rbElms) {
+				rbElm.addEventListener(rbEvtName, eventHandler);
+				angular.element(rbElm).scope().$on('$destroy', () => {
+					rbElm.removeEventListener(rbEvtName, eventHandler);
+				});
+			}
+		}
+
+		/* Public
+		 *********/
+		this.addListeners = (rbElms, rbEvtName) => { // :void
+			manageEvents(rbElms, rbEvtName);
+		}
+
+		this.addListenersFind = ($elmContainer, rbElmName, rbEvtName) => { // :void
+			const rbElms = $elmContainer.querySelectorAll(rbElmName);
+			manageEvents(rbElms, rbEvtName);
+		}
 }]);
