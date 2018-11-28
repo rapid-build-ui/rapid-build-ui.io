6c578246d78c3855880a05f6f6cba0995a75c784
yokun <yevgeniyokun@gmail.com>
2018-11-28 14:02:10 -0600

    feat(checkboxes): add api for validation and multiple value types

--- src/client/scripts/controllers/components/checkboxes.js
+++ src/client/scripts/controllers/components/checkboxes.js
@@ -7,8 +7,9 @@ angular.module('rapid-build').controller('rbCheckboxesController', ['$scope', '$
 			const s = ' ', t = '\t', n = '\n', nt = '\n\t';
 			// const { is } = type;
 			const { data } = $scope;
-			const { disabled, horizontal, inline, label, right, subtext, validation, value } = $scope.a;
+			const { disabled, horizontal, inline, label, labelKey, right, subtext, validation, value } = $scope.a;
 
+			// attrs += `${s}rb-bind`;
 			if (disabled)   attrs += `${s}disabled`;
 			if (horizontal) attrs += `${s}horizontal`;
 			if (inline)     attrs += `${s}inline`;
@@ -16,29 +17,95 @@ angular.module('rapid-build').controller('rbCheckboxesController', ['$scope', '$
 			if (label)      attrs += `${s}label="${label}"`;
 			if (subtext)    attrs += `${s}subtext="${subtext}"`;
 			if (value)      attrs += `${s}value='${JSON.stringify(value)}'`;
-			if (data)       attrs += `${s}data='${JSON.stringify(data)}'`;
-			if (validation && validation.length) attrs += `${s}value='${JSON.stringify(validation)}'`;
+			if (data)       attrs += `${n}data='${buldDataMarkup()}'`;
+			if (validation && validation.length) attrs += `${n}validation='${buldValidationMarkup()}'`;
+			if (labelKey)   attrs += `${n}label-key="${labelKey}"`;
 
 			return `<rb-checkboxes${attrs}></rb-checkboxes>`;
 		};
 
 		/* Public Props
 		 ***************/
-		$scope.data = ['hulk', 'storm', 'thor'];
+		$scope.data = [
+			['batman', 'superman', 'wolverine'],
+			[
+				{id: 1, name: 'batman'},
+				{id: 2, name: 'superman'},
+				{id: 3, name: 'wolverine'}
+			]
+		]
 		$scope.dataLabels = [
 			'array of strings',
 			'array of objects'
 		]
+		$scope.labelKeys = ['name', 'id'];
 		$scope.validationLabels = ['required'];
+		$scope.validations = [
+			'required'
+		]
 
 		/* Public Methods
 		 *****************/
 		$scope.reset = () => {
 			$scope.a = {
 				label: 'Superheroes',
-				value: ['hulk', 'storm'],
-				data: 'array of strings'
+				value: ['batman', 'superman'],
+				data: 'array of strings',
+				// validation: $scope.validations
 			};
+		};
+
+		/* Methods
+		 *********/
+		const stringifyModifier = function(key, val) {
+			val = angular.copy(val);
+			if (!type.is.function(val)) { return val; }
+			return val.toString();
+		};
+
+		const buldValidationMarkup = function() {
+			const validators = [];
+			for (let i = 0; i < $scope.a.validation.length; i++) {
+				const validator = $scope.a.validation[i];
+				switch (validator) {
+					case 'required':
+						validators.push($scope.validations[0]);
+						break;
+					case 'minLength':
+						validators.push($scope.validations[1]);
+						break;
+					case 'minMaxLength':
+						validators.push($scope.validations[2]);
+						break;
+					case 'custom':
+						validators.push($scope.validations[3]);
+						break;
+				}
+			}
+
+			return JSON.stringify(validators, stringifyModifier, '\t')
+				.replace(/\\n/g, '\n')
+				.replace(/\\"/g, '"')
+				.replace(/"function\s*\((.*)\)/g, 'function($1)')
+				.replace(/\}"/g, '}');
+		};
+
+		const buldDataMarkup = function() {
+			let _data = [];
+			switch ($scope.a.data) {
+				case 'array of strings':
+					_data = $scope.data[0];
+					break;
+				case 'array of objects':
+					_data = $scope.data[1];
+					break;
+			}
+
+			return JSON.stringify(_data, stringifyModifier, '\t')
+				.replace(/\\n/g, '\n')
+				.replace(/\\"/g, '"')
+				.replace(/"function\s*\((.*)\)/g, 'function($1)')
+				.replace(/\}"/g, '}');
 		};
 
 		/* Watches
