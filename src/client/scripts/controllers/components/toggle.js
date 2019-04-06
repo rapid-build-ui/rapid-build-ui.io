9c6ed01219113040e74ae454c9374f298fe5c3ef
Judd Younce <jyounce@yahoo.com>
2019-04-06 00:19:13 -0500

    docs(rb-toggle): change api option template-url to fetch

--- src/client/scripts/controllers/components/toggle.js
+++ src/client/scripts/controllers/components/toggle.js
@@ -1,20 +1,20 @@
-angular.module('rapid-build').controller('rbToggleController', ['$scope', '$element', 'preService', 'typeService',
-	function($scope, $element, preService, type) {
+angular.module('rapid-build').controller('rbToggleController', ['$scope', '$element', 'preService',
+	function($scope, $element, preService) {
 		/* Builder
 		 **********/
 		const createMarkup = function() {
 			let attrs = ''; let content = '';
 			const s = ' ', t = '\t', n = '\n', nt = '\n\t';
 
-			if ($scope.a.dark)          attrs += `${nt}dark`; // TODO
-			if ($scope.a.open)          attrs += `${nt}open`;
-			if ($scope.a.cache)         attrs += `${nt}cache`;
-			if ($scope.a.inline)        attrs += `${nt}inline`;
-			if ($scope.a.disabled)      attrs += `${nt}disabled`;
-			if ($scope.a.kind)          attrs += `${nt}kind=\"${$scope.a.kind}\"`;
-			if ($scope.a.caption)       attrs += `${nt}caption=\"${$scope.a.caption}\"`;
-			if ($scope.a.templateUrl)   attrs += `${nt}template-url=\"${$scope.a.templateUrl}\"`;
-			if ($scope.a.action)        attrs += `${nt}${getAction()}`;
+			if ($scope.a.dark)     attrs += `${nt}dark`; // TODO
+			if ($scope.a.open)     attrs += `${nt}open`;
+			if ($scope.a.inline)   attrs += `${nt}inline`;
+			if ($scope.a.disabled) attrs += `${nt}disabled`;
+			if ($scope.a.kind)     attrs += `${nt}kind="${$scope.a.kind}"`;
+			if (!$scope.a.cache)   attrs += `${nt}cache="false"`;
+			if ($scope.a.caption)  attrs += `${nt}caption="${$scope.a.caption}"`;
+			if ($scope.a.fetch)    attrs += `${nt}fetch="/examples/toggle"`;
+			if ($scope.a.onclick)  attrs += `${nt}${getOnclick()}`;
 			if ($scope.a.popover) content += getPopoverSlot();
 			if ($scope.a.content) content += `${nt}${$scope.a.content}${n}`;
 
@@ -27,15 +27,15 @@ angular.module('rapid-build').controller('rbToggleController', ['$scope', '$elem
 			return '\n\t<rb-popover\n\t\tslot="popover"\n\t\tposition="top">\n\t\tmore info...\n\t</rb-popover>\n';
 		}
 
-		const getAction = () => { // :string<function>
-			let action = `
-				function() {
-					console.log('TOGGLED');
-				}
+		const getOnclick = () => { // :string<function>
+			let onclick = `
+				!function() {
+					console.log('toggled');
+				}()
 			`;
 			// return new Promise(resolve => setTimeout(resolve, 3000));
-			action = preService.get.text(action);
-			return `action="${action}"`;
+			onclick = preService.get.text(onclick);
+			return `onclick="${onclick}"`;
 		}
 
 		/* Props
@@ -48,6 +48,7 @@ angular.module('rapid-build').controller('rbToggleController', ['$scope', '$elem
 			$scope.a = {
 				caption: 'Toggle',
 				content: 'Content...',
+				cache: true
 			};
 		};
 
@@ -62,6 +63,18 @@ angular.module('rapid-build').controller('rbToggleController', ['$scope', '$elem
 		const resetFrm = () => $scope.$apply($scope.reset);
 		const resetBtn = $element[0].querySelector('[data-reset]');
 		resetBtn.addEventListener('clicked', resetFrm);
+
+		/* Testing
+		 **********/
+		// const toggle = () => {
+		// 	return new Promise((resolve, reject) => {
+		// 		setTimeout(() => {
+		// 			resolve('<strong>Elementary, My Dear Watson</strong>');
+		// 		}, 2500);
+		// 	});
+		// }
+		// const rbToggle = $element[0].querySelector('rb-toggle');
+		// rbToggle.onclick = toggle;
 
 		/* Init
 		 *******/
