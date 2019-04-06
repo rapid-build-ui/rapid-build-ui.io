8cc5c9348a3df6f700700559612ea0043f3b245a
Judd Younce <jyounce@yahoo.com>
2019-04-05 20:40:58 -0500

    chore(add new server route): to partial file for rb-toggle fetch option

--- src/server/routes/examples.js
+++ src/server/routes/examples.js
@@ -5,6 +5,11 @@ const Routes = (app, opts={}) => {
 	app.get('/examples/bare-bones', (req, res) => {
 		res.sendFile('examples/bare-bones.html', { root: opts.paths.client });
 	});
+
+	// partial for rb-toggle fetch option
+	app.get('/examples/toggle', (req, res) => {
+		res.sendFile('examples/toggle.html', { root: opts.paths.client });
+	});
 }
 
 /* Export it!
